9f25a52f271c86d6e959ce20bdc8c065b0e76bd1
dyuthiramesh <112924506+dyuthiramesh@users.noreply.github.com>
2023-10-12 09:36:03 +0530

    Added prompt variable to .env file

--- test-openai-model.py
+++ test-openai-model.py
@@ -1,3 +1,46 @@
+<<<<<<< HEAD
+import openai
+from dotenv import load_dotenv
+
+# Load environment variables from .env
+load_dotenv()
+
+import os
+
+# Access environment variables
+AZURE_OAI_KEY = os.getenv("AZURE_OAI_KEY")
+AZURE_OAI_ENDPOINT = os.getenv("AZURE_OAI_ENDPOINT")
+AZURE_OAI_MODEL = os.getenv("AZURE_OAI_MODEL")
+PROMPT = os.getenv("PROMPT")
+
+code = '''
+#include <stdio.h>
+
+int main(){
+    print("Hello World")
+    return 0
+}
+'''
+
+# Set OpenAI configuration settings
+openai.api_type = "azure"
+openai.api_base = AZURE_OAI_ENDPOINT
+openai.api_version = "2023-03-15-preview"
+openai.api_key = AZURE_OAI_KEY
+
+# Send request to Azure OpenAI model
+print("Sending request for summary to Azure OpenAI endpoint...\n\n")
+response = openai.ChatCompletion.create(
+    engine=AZURE_OAI_MODEL,
+    temperature=0.7,
+    max_tokens=300,
+    messages=[
+       {"role": "system", "content": PROMPT},
+        {"role": "user", "content": code}
+    ]
+)
+
+=======
 import openai
 from dotenv import load_dotenv
 
@@ -38,4 +81,5 @@ response = openai.ChatCompletion.create(
     ]
 )
 
+>>>>>>> origin/main
 print("Summary: " + response.choices[0].message.content + "\n")
